f93abbb02632183399c4a41cc5a5a5e1d9d33ff8
Kithio <Kithio.Lost@gmail.com>
2019-05-04 15:18:29 +0200

    Updates

---
+++ precache-manifest.083ad576cb567e1fcad13ebe0b78272d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82bb7a8804fa96ca49934d139bf9cbf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e74502e295314b4ef7da",
+    "url": "/static/css/main.77d8b983.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e74502e295314b4ef7da",
+    "url": "/static/js/main.df0391fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
